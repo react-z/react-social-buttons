aec4b8d30b94c26caed2d6926f4dd7c4783f2338
Hemalatha Manickam <hmanicka@gmail.com>
2018-05-09 16:09:54 -0700

    removed appid; upgraded to v3

--- src/FacebookMessengerButton.js
+++ src/FacebookMessengerButton.js
@@ -1,58 +1,58 @@
 import ReactDOM from 'react-dom'
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
 
-export default class FacebookMessengerButton extends React.Component{
+export default class FacebookMessengerButton extends Component {
 
   constructor(props) {
     super(props)
-    this.state = { initalized : false }
+    this.state = { initalized: false }
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.init()
   }
 
-  init () {
-    if(this.state.initalized){
+  init() {
+    if (this.state.initalized) {
       return
     }
 
     let fbsendbutton = ReactDOM.findDOMNode(this.refs.fbsendbutton)
     let fbscript = document.createElement('script')
-    fbscript.src = '//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=230650050380161'
+    fbscript.src = '//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0'
     fbscript.id = 'facebook-jssdk'
     fbscript.onload = this.renderWidget.bind(this)
     fbsendbutton.parentNode.appendChild(fbscript)
 
-    this.setState({initalized: true });
+    this.setState({ initalized: true });
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     let elem = document.getElementById('facebook-jssdk')
-    if(elem !== undefined){
+    if (elem !== undefined) {
       elem.parentNode.removeChild(elem);
     }
   }
 
-  renderWidget(){
-      /*
-         need to detect if it has already been parsed.
-         if coming from react router it may need reparsing.
-      */
-      setTimeout(function () {
-        let elem = document.getElementById('fbsendbutton')
-        if(elem.getAttribute('fb-xfbml-state') === null){
-          FB.XFBML.parse();
-        }
-      }, 1000)
+  renderWidget() {
+    /*
+       need to detect if it has already been parsed.
+       if coming from react router it may need reparsing.
+    */
+    setTimeout(function () {
+      let elem = document.getElementById('fbsendbutton')
+      if (elem.getAttribute('fb-xfbml-state') === null) {
+        FB.XFBML.parse();
+      }
+    }, 1000)
   }
 
-  render(){
+  render() {
     return (
       <div id='fbsendbutton'
-           ref='fbsendbutton'
-           className='fb-send'
-           data-href={this.props.url} >
+        ref='fbsendbutton'
+        className='fb-send'
+        data-href={this.props.url} >
       </div>
     );
 
